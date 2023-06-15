25e757d6bb50a39104ad85dcca7315252d2998fb
learn2impacts dev <learn2impacts@gmail.com>
2023-06-15 21:52:10 +0530

    feat(home): added home page- added courses

--- script.js
+++ script.js
@@ -25,61 +25,74 @@ $(document).ready(function(){
 const courses = [
   {
     id: 1,
-    name: "Course 1",
-    description: "Description of Course 1",
-    totalHours: 30,
+    name: "Computer Basic",
+    description: "Computer overview, words, paint",
+    totalHours: 20,
     timeline: "2 months"
   },
   {
     id: 2,
-    name: "Course 2",
-    description: "Description of Course 2",
-    totalHours: 40,
-    timeline: "3 months"
-  },
-  // Add more courses here
-  {
-    id: 3,
-    name: "Course 3",
-    description: "Description of Course 2",
+    name: "MS Office",
+    description: "Word, Excel, PP",
     totalHours: 40,
     timeline: "3 months"
   },
   {
     id: 3,
-    name: "Course 3",
-    description: "Description of Course 2",
+    name: "JavaScript",
+    description: "Make candidate ready for working in Javascript",
+    totalHours: 30,
+    timeline: "2 months"
+  },
+  {
+    id: 4,
+    name: "Angular",
+    description: "Make candidate ready for working in Angular project",
+    totalHours: 50,
+    timeline: "5 months"
+  },
+  {
+    id: 5,
+    name: "NodeJs",
+    description: "Make candidate ready for working in Node project",
+    totalHours: 50,
+    timeline: "5 months"
+  },
+  {
+    id: 6,
+    name: "Web Development",
+    description: "Make candidate ready for working in web development",
+    totalHours: 50,
+    timeline: "5 months"
+  },
+  {
+    id: 7,
+    name: "Java",
+    description: "Make candidate ready for working in Java project",
+    totalHours: 60,
+    timeline: "5 months"
+  },
+  {
+    id: 8,
+    name: "Database",
+    description: "Make candidate ready for working with Databases",
     totalHours: 40,
     timeline: "3 months"
   },
   {
-    id: 3,
-    name: "Course 3",
-    description: "Description of Course 2",
-    totalHours: 40,
-    timeline: "3 months"
+    id: 9,
+    name: "IT Industrial training",
+    description: "Usage of GIT, IT freshers industrial project",
+    totalHours: 50,
+    timeline: "5 months"
   },
   {
-    id: 3,
-    name: "Course 3",
-    description: "Description of Course 2",
-    totalHours: 40,
-    timeline: "3 months"
-  },
-  {
-    id: 3,
-    name: "Course 3",
-    description: "Description of Course 2",
-    totalHours: 40,
-    timeline: "3 months"
-  },
-  {
-    id: 3,
-    name: "Course 3",
-    description: "Description of Course 2",
-    totalHours: 40,
-    timeline: "3 months"
-  },
+    id: 10,
+    name: "IT Freshers Interview Preparation",
+    description: "It will be a course that includes algorithm, flowchart, one computer language, database, GIT, industrial project, CV development, Job search instructions",
+    totalHours: 80,
+    timeline: "6 months"
+  }
 ];
 
 // Function to create course cards
